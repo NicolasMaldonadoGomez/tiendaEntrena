fb75f4334d5e2e25d21f3a39a15e3a20ade301ff
Nicolas Maldonado Gomez <nicomal@outlook.com>
2022-11-26 05:34:03 -0500

    Se crea otra clase y tabla de ofertas

--- productos/models.py
+++ productos/models.py
@@ -6,3 +6,9 @@ class Producto (models.Model):
     precio = models.FloatField(blank=True, null=True)
     stock  = models.IntegerField(blank=True)
     url_imagen= models.CharField(max_length=2083)
+
+class Oferta (models.Model):
+    codigo = models.CharField(max_length=12)
+    description = models.CharField(max_length=255)
+    descuento  = models.FloatField()
+    
